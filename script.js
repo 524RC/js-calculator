ed850ecc69155f093b7d6522b78ec69048059def
524RC <Rchung524@gmail.com>
2023-10-03 13:04:28 -0400

    java added

---
+++ script.js
@@ -0,0 +1,29 @@
+let prevVal = '';
+let newVal = '';
+let resultVal = '';
+let mathOperator = '';
+
+//store whether or not decimal was clicked 
+//only allow one decimal per val 
+let decimalClicked = false;
+//Hold values we want sotred in memory
+let valMemStored='';
+
+function numButPress(num){
+
+}
+function mathButPress(operator){
+
+}
+function equalButPress(){
+
+}
+function clearButPress(){
+
+}
+function copyButPress(){
+
+}
+function pasteButPress(){
+    
+}
